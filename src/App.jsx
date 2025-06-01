628624c4551fda256b986d4e67cdd1e50c13c40d
jafar mirzapoor <jafarmirzapoor11@gmail.com>
2025-06-01 09:47:44 +0330

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { Route, Routes } from "react-router";
-import Crew from "./components/Crew";
-import Destination from "./components/destination";
-import Homepage from "./components/Homepage";
-import Technology from "./components/Technology";
-const App = () => {
-  return (
-    <Routes>
-      <Route element={<Homepage />} path="/" />
-      <Route element={<Crew />} path="/crew" />
-      <Route element={<Technology />} path="/technology" />
-      <Route element={<Destination />} path="/destination" />
-    </Routes>
-  );
-};
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default App;
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
